5e1d54bd03f7d5b7db531c2fdcb256043374c2f3
BRUGUSOFT <kiran.paturi@brugu.io>
2023-08-07 11:36:35 +0530

    added code

---
+++ ekart/src/App.jsx
@@ -0,0 +1,22 @@
+export const App = () => {
+    //logical section
+
+    //rendering section
+    return (
+        <>
+            <h1>Welcome to react</h1>
+        </>
+    );
+}
+
+export const App2 =() => {
+     //logical section
+
+    //rendering section
+    return (
+        <>
+            <h1>Welcome to react22222</h1>
+        </>
+    );
+}
+
